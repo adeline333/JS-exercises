1ad6a73ffe739ddf3a0012fadb3cf8ca53a2e243
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-02-19 20:12:17 +0200

    added solutions to object exercises in ES6

---
+++ k.js
@@ -0,0 +1,23 @@
+// const sumByCategory = (arr) => {
+//     const result = {};
+  
+//     for (const { category, value } of arr) {
+//       // Check if the category exists in the result object
+//       if (result[category] === undefined) {
+//         result[category] = value;  // If category doesn't exist, set it to the value
+//       } else {
+//         result[category] += value;  // If category exists, add the value to it
+//       }
+//     }
+  
+//     return result;
+//   };     
+
+//   for (let num of numbers){
+
+//   }
+const data = 
+    { category: 'A', value: 10 };
+  let {category} =data;
+  console.log(data);
+  
