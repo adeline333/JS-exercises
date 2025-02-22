b862129d8b9a0758d1d5e734c409d986d18696ef
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-02-22 14:29:29 +0200

    solution to opposite numbers exercise

---
+++ oppositeNum.js
@@ -0,0 +1,13 @@
+function opposite(number) {
+    if(number>0){
+      number= -number;
+    }
+    else if(number<0){
+      number=number*(-1);
+    }
+    else {
+      number=0;
+    }
+    return number;
+   
+  }
