546cd3d63221552a87a26c3556bd271e76afc30e
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-02-23 17:15:11 +0200

    es6 4 object exercises

--- k.js
+++ k.js
@@ -1,23 +1,25 @@
-// const sumByCategory = (arr) => {
-//     const result = {};
+const sumByCategory = (arr) => {
+    const result = {};
   
-//     for (const { category, value } of arr) {
-//       // Check if the category exists in the result object
-//       if (result[category] === undefined) {
-//         result[category] = value;  // If category doesn't exist, set it to the value
-//       } else {
-//         result[category] += value;  // If category exists, add the value to it
-//       }
-//     }
+    for (const { category, value } of arr) {
+      // Check if the category exists in the result object
+      if (result[category] === undefined) {
+        result[category] = value;  // If category doesn't exist, set it to the value
+      } else {
+        result[category] += value;  // If category exists, add the value to it
+      }
+    }
   
-//     return result;
-//   };     
+    return result;
+  };     
 
 //   for (let num of numbers){
 
 //   }
-const data = 
-    { category: 'A', value: 10 };
-  let {category} =data;
-  console.log(data);
+
+
+// const data = 
+//     { category: 'A', value: 10 };
+//   let {category} =data;
+//   console.log(data);
   
