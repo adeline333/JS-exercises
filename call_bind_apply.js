39b12c438af0b8daa6cba15fbc6a46217d5a187c
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-03-14 13:31:43 +0200

    solution to the 2nd exercise using apply

--- call_bind_apply.js
+++ call_bind_apply.js
@@ -4,3 +4,19 @@ function calculateArea(length,width){
 }
 let calculatingArea=calculateArea.bind(null,10);
 console.log(calculatingArea(5));
+
+
+//b
+//b.
+let arrayNum=[34, 12, 57, 28, 91];
+function findingMax(nums){
+  let max=nums[0];
+  for(let i=0;i<nums.length;i++){
+    if (nums[i]>max){
+        max=nums[i];
+    }
+  }
+  return max;
+}
+
+console.log(findingMax.apply(null,[arrayNum]));
