7d7c0d3196309db4defdce430d27998e3234cc01
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-03-25 23:18:15 +0200

    solution to counting sheep execise

---
+++ counting.js
@@ -0,0 +1,13 @@
+function countSheeps(sheep) {
+
+    let count =0;
+    for (let num of sheep){
+      if (num==true){
+        count+=1
+      }
+      else{
+        count==0;
+      }
+    }
+    return count;
+  }
